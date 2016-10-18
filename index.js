20acfc0de738f7b7bc27de095b44149baff0e02e
Ralph <ralph.ewig@audacy.space>
2016-10-17 18:15:48 -0700

    Added login redirect from entry page.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Quindar!')
+	response.writeHead(301,
+		{Location: 'http://whateverhostthiswillbe:8675/'+newRoom}
+		);
+	response.end();
 })
 
 app.listen(app.get('port'), function() {
