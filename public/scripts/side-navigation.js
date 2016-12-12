dae8bb11144854b5c9770c66761fb7c41ccd69be
tarunigattu1992 <taruni.gattu@audacy.space>
2016-12-12 14:34:31 -0800

    committing Responsive Dashboard layout for Mobile and Tablet Screens

--- public/scripts/side-navigation.js
+++ public/scripts/side-navigation.js
@@ -1,51 +1,277 @@
-     function openNav() {
-
        
+
+      /* Global variables to define mobile and tablet widths and height*/
        var mq = window.matchMedia( "(max-width: 375px)" );
+       var mq_nexus_5x = window.matchMedia("(max-width: 412px)");
+       var iphone6pluspotrait = window.matchMedia("(max-width: 414px)");
+       var mq_galaxy_landscape = window.matchMedia("(max-width: 640px)");
+       var mq_height = window.matchMedia("(max-height: 360px)");
+       var mq_nexus_landscape = window.matchMedia("(max-width: 732px)");
+       var mq_nexus_landscape_height = window.matchMedia("(max-height: 412px)");
+       var mq_iphone5_landscape = window.matchMedia("(max-width: 568px)");
+       var mq_iphone5_landscape_height = window.matchMedia("(max-height: 320px)");
+       var mq_iphone6_landscape = window.matchMedia("(max-width: 667px)");
+       var mq_iphone6_landscape_height = window.matchMedia("(max-height: 375px)");
+       var mq_iphone6plus_landscape = window.matchMedia("(max-width: 736px)");
+       var mq_iphone6plus_landscape_height = window.matchMedia("(max-height: 414px)");
+       var mq_iphone6plus_landscape = window.matchMedia("(max-width: 736px)");
+       var mq_iphone6plus_landscape_height = window.matchMedia("(max-height: 414px)");
+       var mq_ipad = window.matchMedia("(max-width: 768px)");
+       var mq_ipad_landscape = window.matchMedia("(max-width: 1024px");
+       var mq_ipad_pro = window.matchMedia("(max-width: 1024px)");
 
-        if(mq.matches){ // works for iphone 6
+     function openLeftNav() {
+        if(mq.matches || mq_nexus_5x.matches || iphone6pluspotrait.matches ){ 
+          document.getElementById("s-icons").style.display = 'none';
           document.getElementById("mySidenav").style.width = "100%";
-        document.getElementById("main").style.marginLeft = "100%";
+          document.getElementById("main").style.marginLeft = "100%";
+          document.getElementById("rclosebtn").style.fontSize ="21px";
+          document.getElementById("main-img").src = '/media/logos/Quindar_Icon_Black.svg';
         }
-        else {
-        document.getElementById("mySidenav").style.width = "18%";
-        document.getElementById("main").style.marginLeft = "18%";
-
+        else if(mq_iphone5_landscape.matches && mq_iphone5_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+         document.getElementById("mySidenav").style.width = "37%";
+         document.getElementById("main").style.marginLeft = "37%";
+         document.getElementById("mySidenav").style.height = "100%";
+         document.getElementById("closebtn").style.width = "100%";
+         document.getElementById("closebtn").style.height = "100%";
+         document.getElementById("closebtn").style.top = "-14px";
+         document.getElementById("closebtn").style.right = "-154px";
+         document.getElementById("closebtn").style.fontSize = "21px";
+          document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+       }
+       else if(mq_galaxy_landscape.matches && mq_height.matches && window.matchMedia("(orientation: landscape)").matches){
+         document.getElementById("mySidenav").style.width = "40%";
+         document.getElementById("main").style.marginLeft = "40%";
+         document.getElementById("mySidenav").style.height = "100%";
+         document.getElementById("closebtn").style.width = "100%";
+         document.getElementById("closebtn").style.height = "100%";
+         document.getElementById("closebtn").style.top ="-14px";
+         document.getElementById("closebtn").style.right ="-204px";
+         document.getElementById("closebtn").style.fontSize ="21px";
+          document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+       }
+       else if(mq_nexus_landscape.matches && mq_nexus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+         document.getElementById("mySidenav").style.width = "38%";
+         document.getElementById("main").style.marginLeft = "38%";
+         document.getElementById("mySidenav").style.height = "100%";
+         document.getElementById("closebtn").style.width = "100%";
+         document.getElementById("closebtn").style.height = "100%";
+         document.getElementById("closebtn").style.top = "-14px";
+         document.getElementById("closebtn").style.right = "-204px";
+         document.getElementById("closebtn").style.fontSize = "21px";
+          document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+       }
+       else if(mq_iphone6plus_landscape.matches && mq_iphone6plus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+         document.getElementById("mySidenav").style.width = "35%";
+         document.getElementById("main").style.marginLeft = "35%";
+         document.getElementById("mySidenav").style.height = "100%";
+         document.getElementById("closebtn").style.width = "100%";
+         document.getElementById("closebtn").style.height = "100%";
+         document.getElementById("closebtn").style.top = "-14px";
+         document.getElementById("closebtn").style.right = "-204px";
+         document.getElementById("closebtn").style.fontSize = "21px";
+          document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+       }
+       else if(mq_ipad.matches){
+          // document.getElementById("s-icons").style.display = 'none';
+         document.getElementById("mySidenav").style.width = "100%";
+         document.getElementById("main").style.marginLeft = "100%";
+         document.getElementById("input-search").style.width = "700px";
+         document.getElementById("rclosebtn").style.fontSize ="21px";
+       }
+      else if(mq_ipad_landscape.matches && window.matchMedia("(orientation: landscape)").matches){
+          // document.getElementById("s-icons").style.display = 'none';
+         document.getElementById("mySidenav").style.width = "30%";
+         document.getElementById("main").style.marginLeft = "30%";
+         document.getElementById("QRegular").style.display = 'none';
+         document.getElementById("rclosebtn").style.fontSize ="21px";
+          // document.getElementById("input-search").style.width = "700px";
+       }
+       else if(mq_ipad_pro.matches){
+         document.getElementById("mySidenav").style.width = "30%";//18%
+         document.getElementById("main").style.marginLeft = "30%";//18%
+         document.getElementById("QRegular").style.display = 'none';
+         document.getElementById("closebtn").style.fontSize ="21px";
+         document.getElementById("input-search").style.fontSize = "18px";
+         document.getElementById("left-menu-item1").style.fontSize = "18px";
+         document.getElementById("left-menu-item2").style.fontSize = "18px";
+         document.getElementById("left-menu-item3").style.fontSize = "18px";
+         document.getElementById("left-menu-item4").style.fontSize = "18px";
+       }
+       else {
+        document.getElementById("mySidenav").style.width = "20%";//18%
+        document.getElementById("main").style.marginLeft = "20%";//18%
+        document.getElementById("input-search").style.width = "400px";
+        document.getElementById("closebtn").style.fontSize ="36px";
+        document.getElementById("input-search").style.fontSize = "18px";
+        document.getElementById("left-menu-item1").style.fontSize = "18px";
+        document.getElementById("left-menu-item2").style.fontSize = "18px";
+        document.getElementById("left-menu-item3").style.fontSize = "18px";
+        document.getElementById("left-menu-item4").style.fontSize = "18px";
+        // document.getElementById("QRegular").style.display = 'none';
       }
-      }
+    }
 
-      function closeNav() {
+    function closeLeftNav() {
+     if(mq.matches || mq_nexus_5x.matches || iphone6pluspotrait.matches ){ 
+       document.getElementById("mySidenav").style.width = "0";
+       document.getElementById("main").style.marginRight = "0";
+       document.getElementById("s-icons").style.display = 'inline-block';
+       document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+     }
+     else if(mq_galaxy_landscape.matches && window.matchMedia("(orientation: landscape)").matches){
+       document.getElementById("mySidenav").style.width = "0";
+       document.getElementById("main").style.marginLeft = "0";
+       document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+     }
+     else if(mq_nexus_landscape.matches && window.matchMedia("(orientation: landscape)").matches){
+       document.getElementById("mySidenav").style.width = "0";
+       document.getElementById("main").style.marginLeft = "0";
+       document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+     }
+     else if(mq_ipad.matches){
+       document.getElementById("s-icons").style.display = 'inline-block';
+       document.getElementById("mySidenav").style.width = "0";
+       document.getElementById("main").style.marginRight = "0";
+     }
+     else if(mq_ipad_pro.matches){
+       document.getElementById("s-icons").style.display = 'inline-block';
+       document.getElementById("mySidenav").style.width = "0";
+       document.getElementById("main").style.marginRight = "0";
+       document.getElementById("QRegular").style.display = 'inline-block';
+     }
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+  }
 
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
+  function openRightNav() {
+    if(mq.matches || mq_nexus_5x.matches || iphone6pluspotrait.matches){ 
+      document.getElementById("s-icons").style.display = 'none';
+      document.getElementById("myrightSidenav").style.width = "100%";
+      document.getElementById("main").style.marginRight = "100%";
+      document.getElementById("rclosebtn").style.fontSize ="21px";
+      document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+    }
+    else if(mq_iphone5_landscape.matches && mq_iphone5_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+     document.getElementById("myrightSidenav").style.width = "37%";
+     document.getElementById("main").style.marginRight = "37%";
+     document.getElementById("myrightSidenav").style.height = "100%";
+     document.getElementById("rclosebtn").style.width = "100%";
+     document.getElementById("rclosebtn").style.height = "100%";
+     document.getElementById("rclosebtn").style.top = "-9px";
+     document.getElementById("rclosebtn").style.right = "-154px";
+     document.getElementById("rclosebtn").style.fontSize = "21px";
+     document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+   }
+   else if(mq_galaxy_landscape.matches && mq_height.matches && window.matchMedia("(orientation: landscape)").matches){
+     document.getElementById("myrightSidenav").style.width = "40%";
+     document.getElementById("main").style.marginRight = "40%";
+     document.getElementById("myrightSidenav").style.height = "100%";
+     document.getElementById("rclosebtn").style.width = "100%";
+     document.getElementById("rclosebtn").style.height = "100%";
+     document.getElementById("rclosebtn").style.top = "-4px";
+     document.getElementById("rclosebtn").style.right = "-198px";
+     document.getElementById("rclosebtn").style.fontSize ="21px";
+     document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+   }
+   else if(mq_nexus_landscape.matches && mq_nexus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+     document.getElementById("myrightSidenav").style.width = "37%";
+     document.getElementById("main").style.marginRight = "37%";
+     document.getElementById("myrightSidenav").style.height = "100%";
+     document.getElementById("rclosebtn").style.width = "100%";
+     document.getElementById("rclosebtn").style.height = "100%";
+     document.getElementById("rclosebtn").style.top = "-4px";
+     document.getElementById("rclosebtn").style.right = "-198px";
+     document.getElementById("rclosebtn").style.fontSize ="21px";
+     document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+   }
+   else if(mq_iphone6plus_landscape.matches && mq_iphone6plus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+     document.getElementById("myrightSidenav").style.width = "37%";
+     document.getElementById("main").style.marginRight = "37%";
+     document.getElementById("myrightSidenav").style.height = "100%";
+     document.getElementById("rclosebtn").style.width = "100%";
+     document.getElementById("rclosebtn").style.height = "100%";
+     document.getElementById("rclosebtn").style.top = "-4px";
+     document.getElementById("rclosebtn").style.right = "-198px";
+     document.getElementById("rclosebtn").style.fontSize ="21px";
+     document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+   }
+   else if(mq_ipad.matches){
+      document.getElementById("top-header").style.display = 'none';
+      document.getElementById("myrightSidenav").style.width = "100%";
+      document.getElementById("main").style.marginRight = "100%";
+      document.getElementById("rclosebtn").style.fontSize ="21px";
+   }
+  else if(mq_ipad_landscape.matches && window.matchMedia("(orientation: landscape)").matches){
+      document.getElementById("myrightSidenav").style.width = "30%";
+      document.getElementById("main").style.marginRight = "30%";
+      document.getElementById("add-widget").style.display = 'none';
+      document.getElementById("username").style.display = 'none';
+      document.getElementById("rclosebtn").style.fontSize ="21px";
+   }
+  else if(mq_ipad_pro.matches) {
+      document.getElementById("myrightSidenav").style.width = "30%";//18%
+      document.getElementById("main").style.marginRight = "30%";//18%
+      document.getElementById("add-widget").style.display = 'none';
+      document.getElementById("username").style.display = 'none';
+      document.getElementById("rclosebtn").style.fontSize ="21px";
+  }
+   else {
+    document.getElementById("myrightSidenav").style.width = "20%";
+    document.getElementById("main").style.marginRight = "20%";
+    document.getElementById("add-widget").style.display = 'none';
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("rclosebtn").style.fontSize ="36px";
+  }
+}
 
-      }
-
-      function openrightNav() {
-
-        var mq = window.matchMedia( "(max-width: 375px)" );
-                if(mq.matches){ 
-          document.getElementById("myrightSidenav").style.width = "100%";
-        document.getElementById("main").style.marginRight = "100%";
-
-
-        }
-        else{
-
-        document.getElementById("myrightSidenav").style.width = "18%";
-        document.getElementById("main").style.marginRight = "18%";
-        document.getElementById("add-widget").style.display = 'none';
-        document.getElementById("username").style.display = 'none';
-
-
-      }
-      }
-
-      function closerightNav() {
-
-        document.getElementById("myrightSidenav").style.width = "0";
-        document.getElementById("main").style.marginRight= "0";
-         document.getElementById("add-widget").style.display = 'inline-block';
-          document.getElementById("username").style.display = 'inline-block';
-         
-      }
+function closeRightNav() {
+  if(mq.matches || mq_nexus_5x.matches || iphone6pluspotrait.matches){ 
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+    document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+  }
+  else if(mq_iphone5_landscape.matches && mq_iphone5_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+    document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+  }
+  else if(mq_galaxy_landscape.matches && mq_height.matches && window.matchMedia("(orientation: landscape)").matches){
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+    document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+  }
+  else if(mq_nexus_landscape.matches && mq_nexus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+    document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+  }
+  else if(mq_iphone6plus_landscape.matches && mq_iphone6plus_landscape_height.matches && window.matchMedia("(orientation: landscape)").matches){
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+    document.getElementById("main-img").src = "/media/logos/Quindar_Icon_Black.svg";
+  }
+  else if(mq_ipad.matches){
+    document.getElementById("top-header").style.display = 'block';
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("username").style.display = 'none';
+    document.getElementById("s-icons").style.display = 'inline-block';
+  }
+  else{
+    document.getElementById("myrightSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("add-widget").style.display = 'inline-block';
+    document.getElementById("username").style.display = 'none';
+  }
+}
