9c3262b370c55b45afd8ef67037d397e1b67d56b
mkakoi <masaki.kakoi@audacy.space>
2017-01-11 13:22:09 -0800

    Zoom in/out feature

--- public/scripts/widgetgrid.js
+++ public/scripts/widgetgrid.js
@@ -27,125 +27,116 @@
 
 			  // function to add ground track widget to the grid layout
               this.addGround = function(){
-				        var parameters = {collectionName : "position"};
+				counter++;
+				var parameters = {collectionName : "position"};
                 var delay = 1000;	// milliseconds
-						    var sc = ["Audacy1", "Audacy2", "Audacy3"];		
+			    var sc = ["Audacy1", "Audacy2", "Audacy3"];		
 									
-				        // Initialize scHolder
-				         var scHolder={}
-				          for (j=0; j<sc.length;j++) {
+				// Initialize scHolder
+				var scHolder={}
+				for (j=0; j<sc.length;j++) {
           				  scHolder[j] = [[0.,0.]];
           				  scHolder[j].pop();
-          				}
+          		}
 					
                 $('.grid-stack').data('gridstack').addWidget($(
-			          '<div class="panel panel-primary" style="margin-bottom:0;">\
+			          '<div class="panel panel-primary" style="margin-bottom:0;" id="divtable'+counter+'">\
 				        <div class="grid-stack-item-content panel-heading" />\
-				        <button id="plotspan">PLOT</button>\
-						    <button type="button" class="close" aria-label="Close" id="removewidget" ><span aria-hidden="true" id="removespan">&times;</span></button>\
-				        <p class="ground-widget"></p>\
-				        <div/>'),0,0,6,5);
+				        <button type="button" class="plotbutton" id="plotbutton'+counter+'">PLOT</button>\
+						<button type="button" class="close" aria-label="Close" id="removewidget" ><span aria-hidden="true" id="removespan">&times;</span></button>\
+					   <div class="svg-container" id="divplot'+counter+'"></div><div/>'),0,0,6,5);
                 
-        				var width = 550;
-        				var height = 300;
-        				var projection = d3.geoEquirectangular()
-        	                           .center([0,0])
-        	                           .scale((width + 1) / 2 / Math.PI)
-        						       .translate([width/2,height/2])
-                                       .precision(.1);			
-        				var path = d3.geoPath()
+        		var projection = d3.geoEquirectangular();		
+        		var path = d3.geoPath()
         						.projection(projection);
-        				var graticule = d3.geoGraticule();
-        				var svg = d3.select("p").append("svg")
-        						.attr("width", width)
-        						.attr("height", height);
-        				var g = svg.append("g");
+        		var graticule = d3.geoGraticule();
+        		var svg = d3.select("#divplot"+counter).append("svg")
+        						  .attr("preserveAspectRatio", "xMinYMin meet")
+                                  .attr("viewBox", "-50 -50 1100 600")
+                                  .classed("svg-content", true);
+        		var g = svg.append("g");
+				g.attr("id","g"+counter);
 
-        				// Plot world map
-        				d3.json("/media/icons/world-110m.json", function(error, world) {
-                          if (error) throw error;	
-                          g.insert("path", ".graticule")
-        					    .datum(topojson.feature(world, world.objects.land))
-        					    .attr("class", "land")
-        					    .attr("d", path);	
+        		// Plot world map
+        		d3.json("/media/icons/world-110m.json", function(error, world) {
+                  if (error) throw error;	
+                  g.insert("path", ".graticule")
+        		   .datum(topojson.feature(world, world.objects.land))
+        		   .attr("class", "land")
+        		   .attr("d", path);	
 
-        				  g.append("path")
-        					    .datum(graticule)
-        					    .attr("d", path)
-        					    .attr("class","graticule");					
-        				});				
+        		  g.append("path")
+        		   .datum(graticule)
+        		   .attr("d", path)
+        		   .attr("class","graticule");					
+        		});				
         						
-                        // Plot data when PLOT button is clicked and keep updating						
-        				$('#plotspan').click(function(){
-                          updatePlot();
-        				});
+                // Plot data when PLOT button is clicked and keep updating						
+        		$('#plotbutton'+counter).click(function(){
+                  timer = setInterval(updatePlot, delay);
+        		});
 
-        					
-        				// Function to update data to be plotted
-        				function updatePlot() {
-        				  $.ajax({  
-        				    url: "/getposition",
-                            type: 'GET',
-                            data: parameters,
-                            success: function(res) {
+        		// Function to update data to be plotted
+        		function updatePlot() {
+        		  $.ajax({  
+        		    url: "/getposition",
+                    type: 'GET',
+                    data: parameters,
+                    success: function(res) {
         						  
-        					// -- Find latest data for each spacecraft --				  
-        					// Initialize latestdata
-        					  var latestdata;
-        						  
-        					  for (j = 0; j < sc.length; j++) {
-        						latestdata = null;	
-        						var i = res.length-1;
-        						// Check if data is available for the selected s/c
-        						while (i > res.length-10) {
-        							  tempdata = res[i]
-        						  if (tempdata.vehicleId.value == sc[j]) {
-        								latestdata = tempdata
-        							    i = i - 10
-        						  } else {
-        							i-- 
-        						  }							  
-        						}
-        						
-                                // Check if the latestdata is available for the selected s/c
-        						if (latestdata == null) {
-        							
-        						} else {
-        					      // update latestdata						  				
-        				          var x = latestdata.x.value;
-        					      var y = latestdata.y.value;
-        					      var z = latestdata.z.value;
-        					
-        					      // Calculate longitude and latitude from the satellite position x, y, z.
-        	                      // The values (x,y,z) must be Earth fixed.
-        	                      r = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-        	                      longitude = Math.atan2(y,x)/Math.PI*180;
-        	                      latitude = Math.asin(z/r)/Math.PI*180;
-        						  console.log(longitude,latitude)
-        						
-        					      // add longitude and latitude to data_plot
-        					      scHolder[j].push([longitude, latitude]);		
+        			  // -- Find latest data for each spacecraft --				  
+        		      // Initialize latestdata
+        			  var latestdata = null;
 
-        					      route = g.append("path")
-                                    .datum({type: "LineString", coordinates: scHolder[j]})	
-                                    .attr("class", "route")
-                                    .attr("d", path);	
-        						  }							
-        					  }
-                    
-                              //-------------------------------------------//		
-        										
-        					  timer = setTimeout(updatePlot, delay);
-        					}
-        				  });
-        				}
-					
+					  console.log(res.length); 				
+        			  latestdata = res[0];      				 
+        						
+                      // Check if the latestdata is available for the selected s/c
+        			  if (latestdata == null) {
+        						
+        			  } else {
+        				  // update latestdata						  				
+        				var x = latestdata.x.value;
+        				var y = latestdata.y.value;
+        				var z = latestdata.z.value;
+        					
+        				// Calculate longitude and latitude from the satellite position x, y, z.
+        	            // The values (x,y,z) must be Earth fixed.
+        	            r = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
+        	            longitude = Math.atan2(y,x)/Math.PI*180;
+        	            latitude = Math.asin(z/r)/Math.PI*180;
+        				console.log(longitude,latitude)
+        						
+        				// add longitude and latitude to data_plot
+        				scHolder[0].push([longitude, latitude]);			
+        				route = g.append("path")
+                                 .datum({type: "LineString", coordinates: scHolder[0]})	
+                                 .attr("class", "route")
+                                 .attr("d", path);							
+        			  }                    
+                      //-------------------------------------------//
+					}					  
+   				  });
+        		}
+				
+                var zoom = d3.zoom()
+				             .scaleExtent([.1,10])
+							 .on("zoom",zoomed);
+							 
+				g.call(zoom);
+				
+				function zoomed(){
+                  var transform = d3.zoomTransform(this);
+                  d3.select("#"+g.attr("id"))
+                  .attr("transform", "translate(" + transform.x + "," + transform.y + ") scale(" + transform.k + ")");
+				};
+				
                 $('.close').click(this.removeWid);
       				  $(document).on('click', 'span', function(e) {
                   e.target.closest("div").remove();
                 });
 
-      			   }.bind(this);
+      	      }.bind(this);
 			      
 
               this.tableWidget = function(){
