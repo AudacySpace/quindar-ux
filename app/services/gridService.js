fa2efad7cf5b5e752961125cd044f3f1bd219153
mkakoi <masaki.kakoi@audacy.space>
2017-03-14 17:50:39 -0700

    lineplot not default

--- app/services/gridService.js
+++ app/services/gridService.js
@@ -113,25 +113,7 @@ app
 				settings: false,
 				saveLoad: false,
 				delete: false,
-            },
-			{
-				col: 0,
-                row: 3,
-				sizeY: 3,
-				sizeX: 4,
-				name: "Line Plot",
-				directive: "lineplot",
-				directiveSettings: "linesettings",
-				id: "addLine",
-				icon: {
-					id: "l-plot",
-					type: "fa-line-chart"
-				},
-				main: true,
-				settings: false,
-				saveLoad: false,
-				delete: false
-			}]
+            }]
         }
     };
 
