2293c7cefada28ef6fd9b99ae555e3dfa905f645
mkakoi <masaki.kakoi@audacy.space>
2017-03-16 19:19:40 -0700

    fix

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
+<<<<<<< bb7d28ad30498db5acfc1d9b3457a32e138bb779
 var app = angular.module('app', ['gridster', 'ui.bootstrap', 'ngMaterial', 'ui.bootstrap.datetimepicker','cgPrompt', 'angularModalService']);
+=======
+var app = angular.module('app', ['gridster', 'ui.bootstrap', 'ngMaterial', 'ui.bootstrap.datetimepicker', 'angularModalService', 'yaru22.md']);
+>>>>>>> temp
