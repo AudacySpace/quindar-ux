a1101c4c94518fb14a69152eb2407c365e887287
tarunigattu1992 <taruni.gattu@audacy.space>
2017-03-20 17:11:32 -0700

    fixed save and close functionality of data table

--- app/directives/datatable/datatablesettings.js
+++ app/directives/datatable/datatablesettings.js
@@ -2,16 +2,18 @@ app.directive('datatablesettings', function() {
   return { 
     restrict: 'E',
     templateUrl:'./directives/datatable/datatablesettings.html', 
-    controller: function($scope){
+    controller: function($scope,datatableService){
 
         $scope.checkedValues = $scope.widget.settings.checkedValues;
-    	
+        var values = angular.copy($scope.checkedValues);
+
         $scope.saveDataTableSettings = function(widget){
             var val = $scope.checkedValues;
             widget.main = true;
             widget.settings.active = false;
             widget.saveLoad = false;
             widget.delete = false;
+            values = angular.copy($scope.checkedValues);
         };
 
         $scope.closeDataTableSettings = function(widget){
@@ -19,6 +21,7 @@ app.directive('datatablesettings', function() {
             widget.settings.active = false;
             widget.saveLoad = false;
             widget.delete = false;
+            datatableService.setCheckedValues(widget,values);
         }
     }
   	}; 
