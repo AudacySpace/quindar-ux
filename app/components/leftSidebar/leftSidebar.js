4d86b1e4d7722693a04108d963b23b6b2a565a16
tarunigattu1992 <taruni.gattu@audacy.space>
2017-04-03 10:32:54 -0700

    working code of search by id in data menu

--- app/components/leftSidebar/leftSidebar.js
+++ app/components/leftSidebar/leftSidebar.js
@@ -3,6 +3,7 @@ app
   	templateUrl: "./components/leftSidebar/left_sidebar.html",
   	controller: function(sidebarService, $interval) {
   		var vm = this;
+        vm.post = {id: null};
 
   		getData();
 
@@ -17,21 +18,74 @@ app
         }
 
         vm.showTelemetryMenu = function(config){
-            config.active = !config.active;
+            config.active = !config.active;            
         }
 
   		vm.selectConfig = function(vehicle, data){
             sidebarService.setVehicleInfo(vehicle.name,data);
   		}
 
+        vm.searchData = function(id){
+
+            var vehs = angular.copy(vm.vehicles);
+            var vehMenu = angular.copy(vm.vehicleMenu);
+            var newObj = {};
+
+            for(var i=0;i<vehs.length;i++){
+                for(var j=0;j<vehs[i].config.length;j++){
+                    for(var k=0;k<vehs[i].config[j].values.length;k++){
+                    if(vehs[i].config[j].active = true){
+                        vehs[i].config[j].active = false;
+                        vehs[i].config[j].datastatus[k] = false;
+                        vm.vehicleMenu = false;
+                    }else {
+                        vehs[i].config[j].active;
+                        vm.vehicleMenu = false;
+                    }
+                }
+            }
+            }
+
+            for(var i=0;i<vehs.length;i++){
+                for(var j=0;j<vehs[i].config.length;j++){
+                    for(var k=0;k<vehs[i].config[j].values.length;k++){
+                        if(id != undefined && id !== '' && id != ''){
+                            if(vehs[i].config[j].values[k].match(id)){
+                                vehMenu = true;
+                                vehs[i].active = true;
+                                vehs[i].config[j].active = true;
+                                vehs[i].config[j].datastatus[k] = true;
+                                newObj = JSON.stringify(vehs);
+                                vm.vehicleMenu = vehMenu;
+                                vm.vehicles = JSON.parse(newObj);
+                            }
+                            else{
+                                vehs[i].config[j].datastatus[k] = false;
+                            }
+                        }
+                    }
+
+                }             
+            }
+        }
+
   		function getData(){
   			sidebarService.getConfig()
   			.then(function(response) {
   				vm.config = response.data;
-                for(var j=0; j<vm.config.length; j++){
-                    vm.config[j].active = false;
-                    vm.config[j].category = initCaps(vm.config[j].category);
+                for(var i=0;i<vm.config.length;i++){
+                    vm.config[i].datastatus = [];
                 }
+
+                for(var j=0; j<vm.config.length; j++){
+                    for(var k=0;k<vm.config[j].values.length;k++){
+                    vm.config[j].active = false;
+                    vm.config[j].datastatus[k] = true;
+                    vm.config[j].category = initCaps(vm.config[j].category);
+                    }
+                }
+
+                console.log(vm.config);
                 
   				vm.vehicles = [ {
 					name : "Audacy1",
@@ -51,18 +105,19 @@ app
 				];
 
        		});
+
   		}
 
         function initCaps(str){
             words = str.toLowerCase().split(' ');
 
-             for(var i = 0; i < words.length; i++) {
-                  var letters = words[i].split('');
-                  letters[0] = letters[0].toUpperCase();
-                  words[i] = letters.join('');
-             }
-             return words.join(' ');
+            for(var i = 0; i < words.length; i++) {
+                var letters = words[i].split('');
+                letters[0] = letters[0].toUpperCase();
+                words[i] = letters.join('');
+            }
+            return words.join(' ');
         }
         
 	}
-})
+});
