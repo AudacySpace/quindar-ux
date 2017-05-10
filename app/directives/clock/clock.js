f196d71a70084474b89824c5ffb2c5e4293fd45f
Chavi Malhotra <Chavi@Chavis-MacBook-Pro.local>
2017-05-10 12:03:23 -0700

    delete clock qwidget rows

--- app/directives/clock/clock.js
+++ app/directives/clock/clock.js
@@ -27,33 +27,39 @@ app.controller('ClockCtrl', function($scope, dashboardService, $interval){
 	updateClock();
 
 	function updateClock(){
-		if($scope.widget.settings.clocks.length > 0) {
-			for (var i=0; i<$scope.widget.settings.clocks.length; i++){
-				if($scope.widget.settings.clocks[i].hasOwnProperty('timezone')) {
-					$scope.widget.settings.clocks[i].time = dashboardService.getTime($scope.widget.settings.clocks[i].timezone);
-				} else {
-					tempTime = dashboardService.countdown($scope.widget.settings.clocks[i].reference);
-					$scope.widget.settings.clocks[i].time = tempTime
-					$scope.widget.settings.clocks[i].delta = tempTime.sign; 
-					if(tempTime.sign == "-"){
-						if(tempTime.days == '000' && tempTime.hours == '00') {
-							if(tempTime.minutes <= '59' && tempTime.minutes > '10') {
-								$scope.widget.settings.clocks[i].style = { color : '#FFC400' };
-							}
-							if(tempTime.minutes <= '10') {
-								$scope.widget.settings.clocks[i].style = { color : '#FF0000' };
-							}
+		for (var i=0; i<$scope.widget.settings.clocks.length; i++){
+			if($scope.widget.settings.clocks[i].hasOwnProperty('timezone')) {
+				$scope.widget.settings.clocks[i].time = dashboardService.getTime($scope.widget.settings.clocks[i].timezone);
+			} else {
+				tempTime = dashboardService.countdown($scope.widget.settings.clocks[i].reference);
+				$scope.widget.settings.clocks[i].time = tempTime
+				$scope.widget.settings.clocks[i].delta = tempTime.sign; 
+				if(tempTime.sign == "-"){
+					if(tempTime.days == '000' && tempTime.hours == '00') {
+						if(tempTime.minutes <= '59' && tempTime.minutes > '10') {
+							$scope.widget.settings.clocks[i].style = { color : '#FFC400' };
 						}
-					} else {
-						$scope.widget.settings.clocks[i].style = { color : 'black' };
+						if(tempTime.minutes <= '10') {
+							$scope.widget.settings.clocks[i].style = { color : '#FF0000' };
+						}
 					}
+				} else {
+					$scope.widget.settings.clocks[i].style = { color : 'black' };
 				}
 			}
-		} else {
-			$interval.cancel($scope.interval);
-		}	
+		}
 	}
 
-	$scope.interval = $interval(updateClock, 1000);
+	$scope.interval = $interval(updateClock, 500);
+
+	$scope.remove = function(clock) {
+		$scope.widget.settings.clocks.splice($scope.widget.settings.clocks.indexOf(clock), 1);
+	}
+
+	$scope.$on("$destroy", 
+		function(event) {
+			$interval.cancel( $scope.interval );
+		}
+	);
 
 })
