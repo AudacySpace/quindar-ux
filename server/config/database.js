67db1319a9f3ab1429f2b6d85d5c817e8d0ebf22
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-07-07 16:26:19 -0700

    database connection string updated

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://qsvr.quindar.space:27017/quindar' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://qsvr.audacy.space:27017/quindar' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
