d4d33ec86b6b76d80bb5f3a8292466166f647b3e
tarunigattu1992 <taruni.gattu@audacy.space>
2017-08-03 13:56:37 -0700

    commiting select mission code

--- app/services/dashboardService.js
+++ app/services/dashboardService.js
@@ -1,5 +1,5 @@
 app
-.factory('dashboardService', ['$interval', '$http', function($interval, $http) {
+.factory('dashboardService', ['$interval', '$http','$uibModal', function($interval, $http,$uibModal) {
     var locks = {
         lockLeft : false,
         lockRight : false
@@ -7,27 +7,69 @@ app
     var telemetry = {};
     var time = "";
     var platforms = [];
-
+    var missions = [];
+    var selectedMission = {"mission":""};
+    var treeData = {"data":[]};
     var icons = {sIcon:"", gIcon:"", pIcon:"",dIcon:""};
     
-
-    getTelemetry();
+    getMissions(missions);
     getProxyStatus();
+    var intervalId = {};
 
-    function getTelemetry() {
+    function getMissions(missions){
+        $http({
+            url: "/getMissions", 
+            method: "GET",
+            params:{}
+        }).then(function success(response) {
+            for(var i=0;i<response.data.length;i++){
+                if(response.data[i] === "Audacy Zero"){
+                    missions.push({"name":response.data[i].mission,"image":"/media/icons/AudacyZero_Logo_Reg.svg"});
+                }else if(response.data[i] === "Audacy Lynq"){
+                    missions.push({"name":response.data[i].mission,"image":""});
+                }else {
+                    missions.push({"name":response.data[i].mission,"image":"/media/icons/Audacy_Icon_White.svg"}); 
+                }
+            }
+            $uibModal.open({
+                templateUrl: './components/dashboard/missionModal.html',
+                controller: 'missionModalCtrl',
+                controllerAs: '$ctrl'
+            }).result.then(function(response){
+                if(response){
+                    getTelemetry(response.name);
+                    getDataMenu(response.name);
+                }
+            },function close(){
+                alert("No mission selected!Reload the page for options.");
+            }); 
+        },function error(response){
+            console.log("No mission available!");
+        });
+    }
+
+    function getTelemetry(missionName) {
         var prevId = "";
-        $interval(function () { 
+        if(intervalId){
+            $interval.cancel(intervalId);
+        }
+        intervalId =  $interval(function () { 
             $http({
                 url: "/getTelemetry", 
                 method: "GET",
-                params: {'mission' : 'ATest'}
+                params: {'mission' : missionName}
             }).then(function success(response) {
-                for(var item in response.data.telemetry){
-                    telemetry[item] = response.data.telemetry[item];
+                if(response.data){
+                    for(var item in response.data.telemetry){
+                        telemetry[item] = response.data.telemetry[item];
+                    }
+                    telemetry['data'] = response.data.telemetry;
+                    telemetry['time'] = response.data.timestamp;
+                    time = response.data.timestamp;
+                }else{
+
                 }
-                telemetry['data'] = response.data.telemetry;
-                telemetry['time'] = response.data.timestamp;
-                time = response.data.timestamp;
+
                 if(isEmpty(response.data) === false){//if data is not empty
                     if(prevId === response.data._id){ //  if proxy application is not receiving any data from ground station
                         icons.sIcon = "grey";
@@ -231,17 +273,89 @@ app
             return null;
         }
     }
+
+    function setCurrentMission(mName){
+        selectedMission.mission = mName.currentMission;
+    }
+    function getCurrentMission(){
+        return selectedMission;
+    }
+
+    function getConfig(missionname) {
+        return $http({
+                url: "/getConfig", 
+                method: "GET",
+                params: {'mission' : missionname}
+            });
+    }
+
+    function getDataMenu(missionname){
+        getConfig(missionname)
+            .then(function(response) {
+                if(response.data) {
+                    treeData.data = getDataTree(response.data);
+                }
+            });
+    }
+
+    //recursive function to create the tree structure data
+    function getDataTree(data, cKey){
+        var tree = [];
+        for(var key in data) {
+            if(data.hasOwnProperty(key)) {
+                var nodes = [];
+                var newKey = (cKey ? cKey + "." + key : key);
+
+                if(typeof data[key] === 'object'){
+                    nodes = getDataTree(data[key], newKey);
+                }
+
+                if(nodes.length != 0) {
+                    key = initCaps(key);
+                }
+
+                var node = {
+                    'name' : key,
+                    'nodes' : nodes,
+                    'value' : newKey,
+                    'active' : false
+                };
+
+                tree.push(node)
+            }
+        }
+        return tree;
+    }
+
+    //function to capitalise the first letter of a string
+    function initCaps(str){
+        words = str.split(' ');
+        for(var i = 0; i < words.length; i++) {
+            var letters = words[i].split('');
+            letters[0] = letters[0].toUpperCase();
+            words[i] = letters.join('');
+        }
+        return words.join(' ');
+    }
     
 	return {
         locks : locks,
         telemetry : telemetry,
+        missions : missions,
         getLock : getLock,
+        setCurrentMission : setCurrentMission,
+        getCurrentMission : getCurrentMission,
+        treeData : treeData,
+        getTelemetry : getTelemetry,
+        getMissions : getMissions,
         setLeftLock : setLeftLock,
         setRightLock : setRightLock,
+        isEmpty : isEmpty,
         icons : icons,
         getTime : getTime,
         countdown : countdown,
         sortObject : sortObject,
-        getData : getData
+        getData : getData,
+        getConfig : getConfig
 	}
 }]);
