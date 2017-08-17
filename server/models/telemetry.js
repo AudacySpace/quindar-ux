2d596940a09942ea1038a87735e3e95228b0fdbd
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-08-17 15:08:14 -0700

    added compound index to telemetry collection

--- server/models/telemetry.js
+++ server/models/telemetry.js
@@ -1,12 +1,15 @@
 // load the things we need
 var mongoose = require('mongoose');
 
-// define the schema for our position model
+// define the schema for our telemetry model
 var telemetrySchema = mongoose.Schema({
     mission : String,
     timestamp : Date,
     telemetry : Object
 }, { collection: 'telemetry' });
+
+// define index on telemetry collection for fast retrieval of data
+telemetrySchema.index({ mission: 1 , timestamp: -1 });
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Telemetry', telemetrySchema);
