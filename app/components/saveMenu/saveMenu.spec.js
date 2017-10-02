82b189fb5867f2d61abdb5edac69747c4b2f6de0
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-10-02 12:15:23 -0700

    added jasmine unit test files and karma configuration

---
+++ app/components/saveMenu/saveMenu.spec.js
@@ -0,0 +1,50 @@
+describe('Testing saveMenu component', function () {
+    var $controller;
+
+    beforeEach(function () {
+        // load the module
+        module('app');
+
+        inject( function($componentController){
+            $controller = $componentController('saveMenu');
+        });
+    });
+
+    it('component controller should be defined', function() {
+    	expect($controller).toBeDefined();
+    });
+
+    it('function closeSaveLoadSettings should be defined', function() {
+    	expect($controller.closeSaveLoadSettings).toBeDefined();
+    });
+
+    it('closeSaveLoadSettings function should close the save and load menu', function() {
+        var widget = {
+            sizeY: 3,
+            sizeX: 4,
+            name: "Line Plot",
+            directive: "lineplot",
+            directiveSettings: "linesettings",
+            id: "addLine",
+            icon: {
+                id: "l-plot",
+                type: "fa-line-chart"
+            },
+            main: false,
+            settings: {
+                active: false,
+                data : {
+                    vehicles : [],
+                    value : "", 
+                    key: ""
+                }
+            },
+            saveLoad: true,
+            delete: false
+        }
+
+        $controller.closeSaveLoadSettings(widget);
+        expect(widget.saveLoad).toEqual(false);
+        expect(widget.main).toEqual(true);
+    });
+});
--- app/services/gridService.js
+++ app/services/gridService.js
@@ -1,6 +1,5 @@
-app
-.factory('gridService', ['$http', '$sessionStorage', '$window', 'userService', 
-    function($http, $sessionStorage, $window, userService) { 
+
+function gridService ($http, $sessionStorage, $window, userService) { 
     var gridsterOptions = {
         margins: [20, 20],
         columns: 8,
@@ -363,4 +362,7 @@ app
         getMissionImage : getMissionImage,
         loadMaps : loadMaps
 	}
-}]);
+}
+
+app
+.factory('gridService', gridService); 
