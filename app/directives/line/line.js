d1d6fe798932fd71609bbc64d5e69ab9e1e240c7
Chavi Malhotra <chavi.malhotra@gmail.com>
2018-01-30 02:48:24 -0800

    dygraph line plot for multiple vehicles with datatype check

--- app/directives/line/line.js
+++ app/directives/line/line.js
@@ -24,7 +24,7 @@ app.controller("LineCtrl", function($scope, $element, $interval, $window, dashbo
         //dateWindow: [0, 1], 
         legend: "always",
         xlabel: "timestamp", 
-        axisLabelWidth : 80,
+        axisLabelWidth : 70,
         xLabelHeight : 16,
         yLabelWidth : 16,
     };
@@ -43,62 +43,103 @@ app.controller("LineCtrl", function($scope, $element, $interval, $window, dashbo
                 var paramY = $scope.widget.settings.data.value;
                 var vehicles = $scope.widget.settings.data.vehicles;
                 var labels = ["time"];
-                var tTemp = parseTime(telemetry['time']);
+                var series = {};
+                var typeFlag = false;
+                if(telemetry['time']){
+                    var tTemp = parseTime(telemetry['time']);
+                    var plotPoint = [tTemp];
 
-                //reset plotData when there is a change in settings
-                if (JSON.stringify(prevSettings) !== JSON.stringify($scope.widget.settings.data)){
-                    for(v in vehicles){
-                        var vehicle = vehicles[v];
+                    //reset plotData when there is a change in settings
+                    if (JSON.stringify(prevSettings) !== JSON.stringify($scope.widget.settings.data)){
                         if(!$scope.plotData){
                             $scope.plotData = new Array();
                         } else {
                             $scope.plotData = [];
                         }
                     }
-                }
 
-                for(var v in vehicles){
-                    var vehicle = vehicles[v];
+                    for(var v in vehicles){
+                        var vehicle = vehicles[v];
 
-                    if(telemetry[vehicle.name] !== undefined){  
-                        var currentData = dashboardService.getData(vehicle.key);
-                        if(currentData){
-                            var xTemp = parseFloat(currentData.value.toFixed(4));
-                            var category = currentData.category;
-                            var yUnits = currentData.units;
-                            $scope.plotData.push([tTemp, xTemp]);
-
-                            if ($scope.plotData.length > 100) {
-                                $scope.plotData.shift();
-                            };
-                        }
-                    }
-
-                    labels.push(vehicle.name);
-                }
-
-                if(category && paramY){
-                    var ylabel = category+" [ "+paramY+ " ] " + yUnits + " "
-                }
-
-                graph.updateOptions({ 
-                    file: $scope.plotData, 
-                    ylabel: ylabel,
-                    xlabel: "timestamp",  
-                    labels: labels,
-                    axes: {
-                        y: {
-                            //drawGrid: true,
-                            valueFormatter: function(y) {
-                                return parseFloat(y.toFixed(4));
-                            },
-                            axisLabelFormatter: function(y) {
-                                return parseFloat(y.toFixed(4));
+                        if(telemetry[vehicle.name] !== undefined){  
+                            var currentData = dashboardService.getData(vehicle.key);
+                            if(currentData){
+                                if(typeof(currentData.value) == "number") {
+                                    var xTemp = parseFloat(currentData.value.toFixed(4));
+                                    var category = currentData.category;
+                                    var yUnits = currentData.units;
+                                    plotPoint.push(xTemp);
+                                } else {
+                                    typeFlag = true;
+                                    break;
+                                }
                             }
                         }
-                    },
-                    drawPoints: true
-                });
+
+                        $scope.plotData.push(plotPoint)
+                        labels.push(vehicle.name);
+                        series[vehicle.name] = {
+                            color : vehicle.color
+                        };
+
+                        if ($scope.plotData.length > 100) {
+                            $scope.plotData.shift();
+                        };
+                    }
+
+                    if(category && paramY){
+                        var ylabel = category+" [ "+paramY+ " ] " + yUnits + " "
+                    }
+
+                    if(typeFlag){
+                        //reset to an empty plot as selected ID is not of type number
+                        graph.updateOptions({
+                            file: [[0]],
+                            axes: {
+                                x: {
+                                    drawGrid: true
+                                },
+                                y: {
+                                    drawAxis: true
+                                }
+                            },
+                            legend: "always",
+                            xlabel: "timestamp",
+                            axisLabelWidth : 70,
+                            xLabelHeight : 16,
+                            yLabelWidth : 16,
+                        });
+                        //reset settings data
+                        $scope.widget.settings.data = {
+                            vehicles : [],
+                            value : "",
+                            key: ""
+                        };
+                        $window.alert(paramY + " is of datatype " + typeof(currentData.value) + 
+                            ". Please select another ID from data menu.");
+                    } else {
+                        graph.updateOptions({
+                            file: $scope.plotData,
+                            ylabel: ylabel,
+                            xlabel: "timestamp",
+                            labels: labels,
+                            axes: {
+                                y: {
+                                    //drawGrid: true,
+                                    valueFormatter: function(y) {
+                                        return parseFloat(y.toFixed(4));
+                                    },
+                                    axisLabelFormatter: function(y) {
+                                        return parseFloat(y.toFixed(2));
+                                    }
+                                }
+                            },
+                            drawPoints: true,
+                            //yRangePad: 200,
+                            series: series
+                        });
+                    }
+                }
 
                 prevSettings = angular.copy($scope.widget.settings.data);
             }
