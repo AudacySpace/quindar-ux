6c28948c2ad3cdff3148619fad3b45dc070628f1
Chavi Malhotra <chavi.malhotra@gmail.com>
2018-03-09 12:10:31 -0800

    update execution test order of jasmine test files with random property as false

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,12 @@
 module.exports = function(config) {
   config.set({
 
+    client: {
+      jasmine: {
+        random: false
+      }
+    },
+
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
