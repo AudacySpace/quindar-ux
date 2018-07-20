443e2e8355965996a457301b4a3f92d2e9127da3
tarunigattu1992 <taruni.gattu@audacy.space>
2018-07-20 16:51:59 -0700

    updated alerts for dashboard,modal,mission modal controllers

--- app/components/dashboard/roleModal.spec.js
+++ app/components/dashboard/roleModal.spec.js
@@ -1,12 +1,13 @@
 describe('Testing role modal controller', function () {
-    var controller, userService, deferredGetRole, deferredAllowed, deferredSetRole, scope;
+    var controller, userService, deferredGetRole, deferredAllowed, deferredSetRole, scope,dashboardService;
     var windowMock = {
         alert: function(message) {
             
         },
         user : {
             role : {}
-        }
+        },
+        document:{}
     };
     var modalInstance = { close: function() {}, dismiss: function() {}, open: function() {} };
     var mission = { missionName : 'ATest', missionImage : '/media/icons/Audacy_Icon_White.svg'};
@@ -17,21 +18,24 @@ describe('Testing role modal controller', function () {
             $provide.value('$window', windowMock);
         });
 
-        inject( function($controller, _$q_, _userService_, $rootScope){
+        inject( function($controller, _$q_, _userService_, $rootScope,_dashboardService_){
             scope = $rootScope.$new();
             userService = _userService_;
+            dashboardService = _dashboardService_;
             deferredGetRole = _$q_.defer();
             deferredSetRole = _$q_.defer();
             deferredAllowed = _$q_.defer();
             spyOn(userService, "getCurrentRole").and.returnValue(deferredGetRole.promise);
             spyOn(userService, "setCurrentRole").and.returnValue(deferredSetRole.promise);
             spyOn(userService, "getAllowedRoles").and.returnValue(deferredAllowed.promise);
+            spyOn(dashboardService,"displayAlert").and.returnValue(true);
 
             controller = $controller('modalCtrl', {
                 $scope : scope,
                 $uibModalInstance: modalInstance,
                 userService: userService,
-                mission: mission
+                mission: mission,
+                dashboardService: dashboardService
             });
             
         });
@@ -121,13 +125,14 @@ describe('Testing role modal controller', function () {
                 callsign : 'VIP'
             }
         }
-
+        var usermessage = 'No mission without the Mission Director. Your role cannot be updated';
         spyOn(windowMock, 'alert');
         spyOn(modalInstance, 'close');
 
         controller.updateRole();
         expect(modalInstance.close).toHaveBeenCalled();
-        expect(windowMock.alert).toHaveBeenCalledWith('No mission without the Mission Director. Your role cannot be updated');
+        expect(dashboardService.displayAlert).toHaveBeenCalledWith(usermessage,"bottom right",'#adminroletoaster',false);
+
     });
 
     it('should update the role of the user when update role is called', function() {
@@ -144,7 +149,7 @@ describe('Testing role modal controller', function () {
 
         spyOn(windowMock, 'alert');
         spyOn(modalInstance, 'close');
-
+        var usermessage = "User's current role updated!";
         deferredSetRole.resolve({ data : {}, status : 200 });
         controller.updateRole();
 
@@ -153,7 +158,7 @@ describe('Testing role modal controller', function () {
 
         expect(userService.setCurrentRole).toHaveBeenCalledWith(controller.role.currentRole, mission.missionName);
         expect(modalInstance.close).toHaveBeenCalled();
-        expect(windowMock.alert).toHaveBeenCalledWith("User's current role updated");
+        expect(dashboardService.displayAlert).toHaveBeenCalledWith(usermessage,"bottom right",'#dashboardtoaster',false);
     });
 
     it('should not update the role of the user when update role is called(response status other than 200)', function() {
