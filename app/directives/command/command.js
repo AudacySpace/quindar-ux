9b14ccb666839552ac2b2d14c19ea25741c6889a
tarunigattu1992 <taruni.gattu@audacy.space>
2018-08-01 19:08:48 -0700

    added code to display latest command response

--- app/directives/command/command.js
+++ app/directives/command/command.js
@@ -69,7 +69,9 @@ app.controller('CommandCtrl',
 
     $scope.sendCommand = function(){   	
     	var time = dashboardService.getTime(0);
-    	$scope.command.sent_timestamp = time.today;
+    	var systemTime = new Date();
+    	var cmdId = systemTime.getTime();
+    	$scope.command.sent_timestamp = cmdId;
     	$scope.command.time = time.utc;
 
     	commandService.saveCommand($scope.email, $scope.command, $scope.mission.missionName)
@@ -85,8 +87,39 @@ app.controller('CommandCtrl',
 		.then(function(response) {
 	        if(response.status == 200) {
 	            $scope.commandLog = response.data;
+
+	            //get all the responses of a command
+	            //find the response with the max timestamp
+	            //assign the status and data to a the commandLog variable
+	            var commandLen = $scope.commandLog.length;
+
+	            for(var i=0;i<commandLen;i++){
+	            	//call function to get the max date;
+	            	//loop through the responses and find the response which matches that response
+	            	//assign to a variable used to display in the command log
+	            	var maxTime = getMaxTime($scope.commandLog[i].response,"gwp_timestamp");
+	            	var responsesLen = $scope.commandLog[i].response.length;
+	            	for(var j=0;j<responsesLen;j++){
+	            		var dateformat = new Date($scope.commandLog[i].response[j].gwp_timestamp);
+	            		if(dateformat.getTime() === maxTime.getTime()){
+	            			$scope.commandLog[i].responseStatus = $scope.commandLog[i].response[j].status;
+	            			$scope.commandLog[i].responseData = $scope.commandLog[i].response[j].metadata_data;
+	            		}
+	            	}
+	            }
 	        }
 	    });
+	}
+
+	function getMaxTime(responses,filter){
+		var timestampArray = [];
+		var maxTime;
+		for(var j=0;j<responses.length;j++){
+	        var dateformat = new Date(responses[j][filter]);
+	        timestampArray.push(dateformat);
+	    }
+	    maxTime = new Date(Math.max.apply(null,timestampArray));
+	    return maxTime;
 	}
 
 	$scope.initialise();
